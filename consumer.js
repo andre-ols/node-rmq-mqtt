11462babd06074804ed33d9912f4b29860eb0803
André Oliveira Silva <badecoandre1999@gmail.com>
2024-03-25 11:06:25 -0300

    feat: create a simple consumer

---
+++ consumer.js
@@ -0,0 +1,19 @@
+const mqtt = require('mqtt');
+
+const client = mqtt.connect(`mqtt://localhost:1883`, {
+    clientId: 'consumer-1',
+    password: 'admin',
+    username: 'admin',
+    clean: false,
+    queueQoSZero: false,
+})
+
+client.on('connect', () => {
+    console.log('Consumer is running');
+    
+    client.subscribe('myTopic');
+
+    client.on('message', (topic, message) => {
+        console.log(`Received message from ${topic}: ${message.toString()}`);
+    })
+})
